299cf7a3769a14e3d75881b953e1122966b6bf1b
ozeki <hhdfgg@users.noreply.github.com>
2019-05-17 13:37:03 +0900

    書こうとした

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,11 +29,118 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.kaomoji = new System.Windows.Forms.Label();
+            this.up = new System.Windows.Forms.Button();
+            this.down = new System.Windows.Forms.Button();
+            this.left = new System.Windows.Forms.Button();
+            this.right = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // kaomoji
+            // 
+            this.kaomoji.AutoSize = true;
+            this.kaomoji.Location = new System.Drawing.Point(113, 112);
+            this.kaomoji.Name = "kaomoji";
+            this.kaomoji.Size = new System.Drawing.Size(49, 12);
+            this.kaomoji.TabIndex = 0;
+            this.kaomoji.Text = "(°ω°)";
+            this.kaomoji.Click += new System.EventHandler(this.kaomoji_Click);
+            // 
+            // up
+            // 
+            this.up.Location = new System.Drawing.Point(147, 167);
+            this.up.Name = "up";
+            this.up.Size = new System.Drawing.Size(75, 23);
+            this.up.TabIndex = 1;
+            this.up.Text = "up";
+            this.up.UseVisualStyleBackColor = true;
+            this.up.Click += new System.EventHandler(this.up_Click);
+            // 
+            // down
+            // 
+            this.down.Location = new System.Drawing.Point(147, 226);
+            this.down.Name = "down";
+            this.down.Size = new System.Drawing.Size(75, 23);
+            this.down.TabIndex = 2;
+            this.down.Text = "down";
+            this.down.UseVisualStyleBackColor = true;
+            this.down.Click += new System.EventHandler(this.down_Click);
+            // 
+            // left
+            // 
+            this.left.Location = new System.Drawing.Point(87, 196);
+            this.left.Name = "left";
+            this.left.Size = new System.Drawing.Size(75, 23);
+            this.left.TabIndex = 3;
+            this.left.Text = "left";
+            this.left.UseVisualStyleBackColor = true;
+            this.left.Click += new System.EventHandler(this.left_Click);
+            // 
+            // right
+            // 
+            this.right.Location = new System.Drawing.Point(197, 197);
+            this.right.Name = "right";
+            this.right.Size = new System.Drawing.Size(75, 23);
+            this.right.TabIndex = 4;
+            this.right.Text = "right";
+            this.right.UseVisualStyleBackColor = true;
+            this.right.Click += new System.EventHandler(this.right_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 19);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "0";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(122, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 19);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "0";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.right);
+            this.Controls.Add(this.left);
+            this.Controls.Add(this.down);
+            this.Controls.Add(this.up);
+            this.Controls.Add(this.kaomoji);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label kaomoji;
+        private System.Windows.Forms.Button up;
+        private System.Windows.Forms.Button down;
+        private System.Windows.Forms.Button left;
+        private System.Windows.Forms.Button right;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,46 @@ namespace hukusyu0517
         {
             InitializeComponent();
         }
+
+        private void kaomoji_Click(object sender, EventArgs e)
+        {
+            //kaomoji.Top += (textBox2.Text);
+        }
+
+        private void up_Click(object sender, EventArgs e)
+        {
+            //textBox2.TextChanged += 1;
+        }
+
+        private void down_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void left_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void right_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //int TextBox1_TextChanged;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            kaomoji.Left += int.Parse(textBox1.Text);
+            kaomoji.Top += int.Parse(textBox2.Text);
+        }
     }
 }
